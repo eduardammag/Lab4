313a7dc52b81b54ca592ba0aea5d52725415a129
Eduarda Mesquita <eduardammag@gmail.com>
2025-03-20 12:33:57 -0300

    Lab4

---
+++ my-portfolio/src/routes/contact/+page.svelte
@@ -0,0 +1,8 @@
+        <form action="mailto:foo@example.com" method="GET">
+            <label for="email">Email: <input id="email" type="email" name="text_email"> </label>
+            <label for="subject">Subject: <input id="subject" type="text" name="text_subject"> </label>
+            <label for="body">Body: <textarea id="body" name="text_message"></textarea> </label>
+
+            <input type="submit" value="Submit" id="button">
+            <!-- <button>Submit</button> -->
+        </form>
